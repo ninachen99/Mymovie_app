920b94c4a01f40a78847914ea0f8da7dbf200329
Nina Chen <ninacode99@gmail.com>
2016-03-03 10:25:04 -0600

    Update order model validation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,5 @@
 class OrdersController < ApplicationController
-	# for the order limit
-	require 'order.rb'
-
-	# end order limit
+	
 	def new
 		@order = Order.new
 	end 
@@ -10,40 +7,24 @@ class OrdersController < ApplicationController
 	def index
 		@orders = Order.all
 	end 
-    # call the validate_orders here
+   
     def create
     	@order = Order.new(order_params)
     	if @order.save 
     		flash[:success] = "Thank you for the order."
     		redirect_to '/orders'
-    # call validate_orders here
-        elsif @order.save == false
+    
+        else
         	render 'new'
-    	else
-    		redirect_to :validate_orders
+    	
     	end 
     end 
    
-
- # setting order limit in orders controller!!!
-  
-    def validate_orders
-        
-		if Order.order_total >= Order.order_limit
-			
-			order.errors[:order] << "Sorry, seats are sold out."
-			
-		end
-
-	end 
-
-
- # end of setting order limit!!!
 
 	private
 	  def order_params
 	  	params.require(:order).permit(:age, :first_name, :last_name, :email, :credit_card, :expiration, :order_quantity)
 	  end 
-  end
+end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,23 +2,32 @@ class Order < ActiveRecord::Base
 	belongs_to :movie
 	belongs_to :theater
 	validates :age, :first_name, :last_name, :email, :credit_card, :expiration, :order_quantity, presence: true
+    validate :orders_sold_out, on: :create
     
 	# order limit validate in models - order.rb!!!
      
 
-	def order_total
-        @orders.each do |order|
-            @order_sum += order.order_quantity
+	def order_total(orders)
+        order_sum = 0
+        orders.each do |order|
+            order_sum += order.order_quantity
+        end 
+        order_sum
     end 
-    
 
-
-    def order_limit
-    	@movie.theaters.each do |theater|
+    def order_limit(movie.theaters)
+    	movie.theaters.each do |theater|
     		@available_seats += (theater.seats.count - @order_sum)
+        end 
     end 
     
+    def orders_sold_out
+
+        if order_total >= order_limit
+
+            errors.add(:order_quantity, "all seats are sold out for this movie.")  
+        end
+    end 
+
 	# end of order limit validate!!!
-  end 
- end 
 end
