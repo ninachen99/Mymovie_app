26b7530ca5fe13b5966c6eab9d8486d2ee9a2033
Nina Chen <ninacode99@gmail.com>
2016-03-03 19:50:03 -0600

    Update order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,15 @@
 class Order < ActiveRecord::Base
 	belongs_to :movie
 	belongs_to :theater
-	validates :age, :first_name, :last_name, :email, :credit_card, :expiration, :order_quantity, presence: true
-    validate :orders_sold_out
+	validates :age, :first_name, :last_name, :email, :credit_card, :expiration, presence: true
+    validate :order_quantity, if: :orders_sold_out?
     
 	# order limit validate in models - order.rb!!!
     # get total order
+    
     def order_total
-        all_orders = Order.all
-        puts all_orders.count
-        
         order_sum = 0
-        all_orders.each do |order|
+        Order.all.each do |order|
             order_sum += order.order_quantity
         end 
         order_sum
@@ -26,7 +24,7 @@ class Order < ActiveRecord::Base
     end 
     
     # compare order total with seats left
-    def orders_sold_out
+    def orders_sold_out?
 
         if order_total >= order_limit
            errors.add(:order_quantity, "all seats are sold out for this movie.")  
